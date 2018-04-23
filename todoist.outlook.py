1fb6291cb2ca58034975cedae95aaea47fdafcd1
Nikita Pchelin <nikita.pchelin@gmail.com>
2018-04-23 09:57:30 -0400

    upgrading libraries, adding virtual environment, using different way to iterate through tasks

--- todoist.outlook.py
+++ todoist.outlook.py
@@ -41,7 +41,7 @@ def pyWinDate2datetime(now_pytime):
       minute=now_pytime.minute,
       second=now_pytime.second
     )
-    
+
     return now_datetime
 
 def main():
@@ -51,7 +51,7 @@ def main():
 
     # Init Todoist API.
     api = todoist.TodoistAPI(config["TODOIST_API_TOKEN"])
-    api.sync(resource_types=['all'])
+    api.sync()
 
     # For some background on Outlook Interop model, see:
     # https://msdn.microsoft.com/en-us/library/office/ff861868%28v=office.15%29.aspx
@@ -61,47 +61,43 @@ def main():
     ns = outlook.GetNamespace("MAPI")
     todo_folder = ns.GetDefaultFolder(olFolderTodo)
     todo_items = todo_folder.Items
+    tasks = todo_items.Restrict("[Complete] = FALSE")
 
-    message = todo_items.GetFirst()
-    total = 0
+    if (len(tasks) == 0):
+        logger.info("No flagged emails found.")
+    else:
+        logger.info("{0} flagged emails will be processed.".format(len(tasks)))
 
-    # Iterate through messages.
-    while message:
-        try:        
-            start_date = pyWinDate2datetime(message.TaskStartDate)
-            end_date = pyWinDate2datetime(message.TaskCompletedDate)
+    for task in tasks:
+        try:
+            creation_date = pyWinDate2datetime(task.CreationTime)
 
-            # Tasks that are not completed in Outlook receive a completion date
-            # in the future something like 4501-01-01.
-            difference_in_years = relativedelta(end_date, start_date).years
-
-            # If the completion date is recent, then it's actually a completed
-            # task and we don't need to generate a task item for it.
-            if (difference_in_years < 10):
-                message = todo_items.GetNext()
+            # Do not create reminders for emails that came in today.
+            if not creation_date.date() < datetime.today().date():
+                print("Skipped, since it was created today.")
                 continue
 
         except AttributeError as ex:
+            logger.exception("Exception occured while processing the message.", ex)
             message = todo_items.GetNext()
-            continue  
-     
+            continue
+
         # How Todoist generates an ID for an Outlook email:
         # https://todoist.com/Support/show/30790/
 
         # How to get PR_INTERNET_MESSAGE_ID for an Outlook item:
-        # http://www.slipstick.com/developer/read-mapi-properties-exposed-outlooks-object-model/           
-        PR_INTERNET_MESSAGE_ID = message.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x1035001E")
-        todoist_message_id = base64.b64encode(bytes("id={0};mid={1}".format(message.EntryID, PR_INTERNET_MESSAGE_ID), "utf-8")).decode("utf-8")
-        
+        # http://www.slipstick.com/developer/read-mapi-properties-exposed-outlooks-object-model/
+        PR_INTERNET_MESSAGE_ID = task.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x1035001E")
+        todoist_message_id = base64.b64encode(bytes("id={0};mid={1}".format(task.EntryID, PR_INTERNET_MESSAGE_ID), "utf-8")).decode("utf-8")
+
         # Add a task.
-        item = api.items.add(u'[[outlook=id3={0}, Review Email: {1}]]'.format(todoist_message_id, message.Subject), config["TODOIST_PROJECT_ID_OUTLOOK"], date_string="today")
-        logger.info("Processing '{0}'.".format(message.Subject.encode("utf-8")))
+        item = api.items.add(u'[[outlook=id3={0}, Review Email: {1}]]'.format(todoist_message_id, task.Subject), config["TODOIST_PROJECT_ID_OUTLOOK"], date_string="")
+        logger.info("Processing '{0}'.".format(task.Subject.encode("utf-8")))
         r = api.commit()
 
         # If error occures, move on.
         if "error_code" in r:
-            logger.info ("Task for email '{0}' was not created. Error: {1}:{2}".format(message.Subject, r["error_code"], r["error_string"]))
-            message = todo_items.GetNext()
+            logger.info ("Task for email '{0}' was not created. Error: {1}:{2}".format(task.Subject, r["error_code"], r["error_string"]))
             continue
 
         # Add a note.
@@ -109,17 +105,8 @@ def main():
         r = api.commit()
 
         # Mark message as read.
-        message.TaskCompletedDate = message.TaskStartDate
-        message.Save()
-
-        # Advanced to the next message.
-        message = todo_items.GetNext()        
-        total += 1
-
-    if (total == 0):
-        logger.info("No flagged emails found.")
-    else:
-        logger.info("{0} flagged emails processed.".format(total))
+        task.TaskCompletedDate = task.CreationTime
+        task.Save()
 
 if __name__=="__main__":
     main()
